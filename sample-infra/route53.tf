7554067ecc801b85301268ad392d5357103f8c73
mountaincenter <y.hiroyuki560730@gmail.com>
2023-03-16 19:21:07 +0900

    create acm alb redirect

--- sample-infra/route53.tf
+++ sample-infra/route53.tf
@@ -4,11 +4,38 @@ data "aws_route53_zone" "naked" {
 
 resource "aws_route53_record" "sample_route53" {
   zone_id = data.aws_route53_zone.naked.zone_id
-  name    = data.aws_route53_zone.naked.name
+  name    = var.fqdn_name
   type    = "A"
   alias {
     name                   = aws_alb.sample_alb.dns_name
     zone_id                = aws_alb.sample_alb.zone_id
     evaluate_target_health = true
   }
+}
+
+resource "aws_acm_certificate" "main" {
+  domain_name       = var.fqdn_name
+  validation_method = "DNS"
+}
+
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.main_amc_c : record.fqdn]
+}
+
+resource "aws_route53_record" "main_amc_c" {
+  for_each = {
+    for d in aws_acm_certificate.main.domain_validation_options : d.domain_name => {
+      name   = d.resource_record_name
+      record = d.resource_record_value
+      type   = d.resource_record_type
+    }
+  }
+  zone_id         = data.aws_route53_zone.naked.id
+  name            = each.value.name
+  type            = each.value.type
+  ttl             = 172800
+  records         = [each.value.record]
+  allow_overwrite = true
 }
